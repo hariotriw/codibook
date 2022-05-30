f6930cf9c8e0dc5089596ef69b7d0cffdc17fe4e
Wowo <wowovanbothen@gmail.com>
2022-05-30 23:30:17 +0700

    admin confirm order

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -242,6 +242,53 @@ class AdminController {
             res.json(err)
         }
     }
+
+    // --- fungsi untuk admin confirm order ---
+    static async confirmOrder(req, res){
+        try {
+            console.log("order cart")
+            const access_token = req.headers['access-token']
+            const verifyToken = jwt.tokenVerifier(access_token, 'secret')
+            const UserId = verifyToken.id
+            const role = verifyToken.role
+            if(role === "admin"){
+                let {shopCartId, UserId, OrderName} = req.body
+                // console.log(shopCartId);
+                let shoppingCart = await ShoppingCart.findOne({
+                    where: {
+                        UserId: UserId, status: "closed", id: shopCartId
+                    }
+                })
+                if (shoppingCart === null){
+                    // Jika shopping cart tidak ditemukan
+                    res.status(500).json("invalid shopping cart")
+                } else {
+                    // Jika shopping cart ditemukan
+                    console.log("shopping cart found");
+                    
+                    let order = await Order.update({
+                        status: "shipping"
+                    },{
+                        where: {
+                            name: OrderName, status: "paid", UserId: UserId
+                        }
+                    })
+                    if(order[0] === 1){
+                        console.log("berhasil mengkonfirmasi Order");
+                        res.json("cek console log backend")
+                    } else {
+                        res.status(500).json("error while confirming order..")
+                    }
+                    
+                    
+                }
+            } else {
+                res.status(403).json("You don't have access to use this features..")
+            }
+        } catch (err) {
+            res.json(err)
+        }
+    }
 }
 
 module.exports = AdminController;
--- backend/routes/api/admin.js
+++ backend/routes/api/admin.js
@@ -2,6 +2,7 @@ const {Router} = require('express')
 const adminRoutes = Router()
 const {AdminController} = require('../../controllers')
 
+// Products
 // adminRoutes.get("/", AdminController.index)
 adminRoutes.get("/products/lists", AdminController.index)
 adminRoutes.post("/products/create", AdminController.store)
@@ -9,6 +10,9 @@ adminRoutes.get("/products/show/:id", AdminController.show)
 adminRoutes.get("/products/edit/:id", AdminController.edit)
 adminRoutes.post("/products/update", AdminController.update)
 adminRoutes.post("/products/delete", AdminController.destroy)
+
+// Orders
+adminRoutes.post("/orders/confirmOrder", AdminController.confirmOrder)
 
 // adminRoutes.get("/testing", AdminController.test)
 // adminRoutes.post("/testing", AdminController.test)
